4a3e9e9314098144b6e6ac823304d4cb825d6401
maiia <mayaprr@yandex.ru>
2022-08-27 16:21:51 +0300

    add quantile approach

--- wrapper.py
+++ wrapper.py
@@ -5,6 +5,7 @@ from os.path import exists
 import numpy as np
 import pandas as pd
 import os
+from scipy.spatial import distance
 
 from fedot.core.composer.metrics import MAE
 from fedot.core.pipelines.pipeline import Pipeline
@@ -20,8 +21,41 @@ ROOT_PATH_DATA = os.path.join(os.getcwd(), 'data')
 
 class FedotWrapper:
     def __init__(self):
+        self.approach = 'quantile'
         self.train_ts = pd.read_excel(os.path.join(ROOT_PATH_DATA, 'Train.xlsx'))
         self.path_to_models_params = os.path.join(os.getcwd(), 'pipelines')
+
+    @staticmethod
+    def get_quantiles_vector_for_ts(time_series: np.ndarray) -> np.ndarray:
+        """ Get quantile vector describing current ts """
+        quantiles_vector = []
+        windows_num = 6
+        ts_parts = np.array_split(time_series, windows_num)
+        for q in [0.25, 0.5, 0.75]:
+            for part in ts_parts:
+                quantile = np.quantile(part, q)
+                quantiles_vector.append(quantile)
+        return np.array(quantiles_vector)
+
+    def get_nearest_ts_quantile_name(self, ts: np.ndarray):
+        """ Get name of column which consist train ts nearest to current by cosine dist """
+        distances = []
+        time_series = self._cut_train_from_test(time_series=ts)
+        cur_vector = self.get_quantiles_vector_for_ts(time_series)
+        for column in range(1, self.train_ts.shape[1]):
+            train_vector = self.get_quantiles_vector_for_ts(self.train_ts.iloc[:, column].values[1:])
+            distances.append(distance.cosine(cur_vector, train_vector))
+        index_min_cosine_dist = distances.index(min(distances))
+        nearest_ts_quantile_name = self.train_ts.columns[index_min_cosine_dist]
+        return nearest_ts_quantile_name
+
+    def _cut_train_from_test(self, time_series: np.ndarray):
+        """ Cut out 'Forecast' elements from test data """
+        horizont = self._get_horizons_to_predict(df=pd.DataFrame(time_series))[0]
+        if horizont == 0:
+            return time_series
+        time_series = time_series[:-horizont]
+        return time_series
 
     def get_ts_name_with_most_correlation(self, time_series: np.ndarray) -> Tuple[str, pd.Series]:
         """ Get name of ts which has the biggest correlation with specified ts"""
@@ -72,10 +106,13 @@ class FedotWrapper:
 
                 test_number = self._get_test_number(file_name=file)
 
-                ts_name_with_max_corr, ts_with_max_corr = \
-                    self.get_ts_name_with_most_correlation(time_series=df[column].values)
+                if self.approach == 'quantile':
+                    column_name = self.get_nearest_ts_quantile_name(ts=df[column].values)
+                elif self.approach == 'correlation':
+                    column_name, _ = \
+                        self.get_ts_name_with_most_correlation(time_series=df[column].values)
 
-                pipeline = self._get_pipeline(ts_name_with_max_corr=ts_name_with_max_corr)
+                pipeline = self._get_pipeline(column_name=column_name)
 
                 horizon = self._get_horizons_to_predict(pd.DataFrame(df[column].values))[0]
 
@@ -134,23 +171,23 @@ class FedotWrapper:
         number = file_name.split('.xlsx')[0].split('Test_example')[1]    # for local testing
         return int(number)
 
-    def _get_pipeline(self, ts_name_with_max_corr: str) -> Pipeline:
+    def _get_pipeline(self, column_name: str) -> Pipeline:
         """ Get pipeline with the biggest correlation """
         pipeline = None
         for file in os.listdir(self.path_to_models_params):
-            ts_name_with_max_corr = ts_name_with_max_corr.replace('/', '')[:-1]
-            if ts_name_with_max_corr in file:
+            column_name = column_name.replace('/', '')[:-1]
+            if column_name in file:
                 model_dir = os.path.join(self.path_to_models_params, file)
                 for model_file in os.listdir(model_dir):
                     if model_file.endswith('.json'):
                         pipeline = Pipeline().from_serialized(source=os.path.join(model_dir, model_file))
         return pipeline
 
-    @staticmethod
-    def _save_result(test_number: int, result_df: pd.DataFrame):
+    def _save_result(self, test_number: int, result_df: pd.DataFrame):
+        """ Saves full ts with preds in xlsx """
         result_file_name = f'Test_output_{test_number}.xlsx'
 
-        result_path = os.path.join(ROOT_PATH_DATA, 'results')
+        result_path = os.path.join(ROOT_PATH_DATA, 'results', self.approach)
         if not exists(result_path):
             os.makedirs(result_path)
 
@@ -159,13 +196,12 @@ class FedotWrapper:
         print(f'Results were saved to: {path_to_file}')
         result_df.to_excel(path_to_file)
 
-    @staticmethod
-    def _visualize_preds(time_series: np.ndarray, forecast: np.ndarray, horizon: int,
+    def _visualize_preds(self, time_series: np.ndarray, forecast: np.ndarray, horizon: int,
                          test_number: int, column: str):
         plt.plot(time_series)
         plt.plot(np.arange(len(time_series) - horizon, len(time_series)), forecast)
         plt.grid()
-        path_to_save = os.path.join(os.getcwd(), 'visualizations')
+        path_to_save = os.path.join(os.getcwd(), 'visualizations', self.approach)
         if not exists(path_to_save):
             os.makedirs(path_to_save)
         plt.savefig(os.path.join(path_to_save, f'{test_number}_{column}.png'))
